3e6826f72b6146e4f33fff8d59d8f9d7dbe2aac5
Araldius <givenmorthen@gmail.com>
2025-12-02 20:49:06 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,29 @@
 package com.example.test_lab_week_12
 
 import com.example.test_lab_week_12.api.MovieService
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "543a6757e1f1bacd41456d8e1149ea83"
 
+    // LiveData that contains a List of movies
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    // LiveData that contains an error message
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (exception: Exception) {
+            errorLiveData.postValue("An error occurred: ${exception.message}")
+        }
+    }
 }
--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
 
+    val popularMovies: LiveData<List<Movie>>
+        get() = movieRepository.movies
+
+    val error: LiveData<String>
+        get() = movieRepository.error
+
+    private fun fetchPopularMovies() {
+        // Launch a coroutine in viewModelScope
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+        }
+    }
 }
