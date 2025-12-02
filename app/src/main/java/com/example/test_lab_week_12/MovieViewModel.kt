8c366dd37b5de7b07e8329bcef9ef672f202a1bb
Araldius <givenmorthen@gmail.com>
2025-12-02 20:49:41 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -3,6 +3,7 @@ package com.example.test_lab_week_12
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
